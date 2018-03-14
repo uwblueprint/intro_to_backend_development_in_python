5ae90db6cc0d68e9d979a57890566233227e632c
Christopher Luc <chris.luc93@gmail.com>
2018-03-14 18:20:30 -0400

    Removed posts data

--- app.py
+++ app.py
@@ -53,24 +53,3 @@ def blogpost(id=None):
 def newpost():
   return render_template('newpost.html')
 
-
-posts = [
-  {
-    'id': 0,
-    'title': 'What\'s poppin\'?',
-    'body': "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    'date': 'March 4, 2018',
-  },
-  {
-    'id': 1,
-    'title': 'Top 10 Taylor Swift Albums',
-    'body': '1. Fearless\n2. 1989\n3. Red\n4. Speak Now\n5. Taylor Swift',
-    'date': 'March 8, 2018',
-  },
-  {
-    'id': 2,
-    'title': 'Dogs vs Cats',
-    'body': 'Dog dog dog cat cat cat',
-    'date': 'March 11, 2018',
-  },
-]
