371e83201481d1212301350fc18d0cb19b4ce918
Abhijeet Prasad <abhijeetprasad98@gmail.com>
2018-03-14 18:24:57 -0400

    Add models.py

--- app.py
+++ app.py
@@ -7,25 +7,11 @@ from flask import (
 )
 import os
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-import sys
+from datetime import datetime 
+
+from models import BlogPost
 
 app = Flask(__name__)
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class BlogPost(db.Model):
-  id = db.Column(db.Integer, primary_key=True)
-  title = db.Column(db.String(64))
-  body = db.Column(db.Text)
-  date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-
-  def __repr__(self):
-    return '<BlogPost {}>'.format(self.body)
 
 @app.route('/')
 def index():
